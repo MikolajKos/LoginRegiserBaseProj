0a9448e5cf9473694ec6e2cd31b4a221f9301756
Mikołaj <95126739+MikolajKos@users.noreply.github.com>
2022-12-06 17:15:45 +0100

    Add files via upload

---
+++ LoginPanelBase/LoginPanelBase/Model/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginPanelBase.Model
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source = LoginRegisterDb.db");
+        }
+    }
+}
---
+++ LoginPanelBase/LoginPanelBase/ViewModels/MainViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using UnitsConverterApp.Commands;
+
+namespace LoginPanelBase.ViewModels
+{
+    public class MainViewModel : ObservableObject
+    {
+        private string _password;
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+
+        private string _login;
+        public string Login
+        {
+            get
+            {
+                return _login;
+            }
+            set
+            {
+                _login = value;
+                OnPropertyChanged(nameof(Login));
+            }
+        }
+
+
+
+        private ICommand _loginCommand;
+        public ICommand LoginCommand
+        {
+            get
+            {
+                if (_loginCommand == null) _loginCommand = new RelayCommand(
+                    (object o) =>
+                    {
+                        
+                    });
+                return _loginCommand;
+            }
+        }
+
+        private ICommand _registerCommand;
+        public ICommand RegisterCommand
+        {
+            get
+            {
+                if (_registerCommand == null) _registerCommand = new RelayCommand(
+                    (object o) =>
+                    {
+
+                    });
+                return _registerCommand;
+            }
+        }
+    }
+}
